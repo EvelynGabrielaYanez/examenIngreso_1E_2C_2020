e62db20806659945f72e93e1db16886f5fee303f
EvelynYanez <evelyn.gabriela.yanez@gmail.com>
2020-08-10 19:39:36 -0300

    ejercicio 1

--- dos.js
+++ dos.js
@@ -7,5 +7,66 @@ b)la marca del más caro de los líquidos
 c)la marca del más barato de los sólidos
 */function mostrar()
 {
-  alert("dos");
+  let vMarca;
+  let vPrecio;
+  let vPeso;
+  let vTipo;
+  let vRespuesta;
+  let vAcumPesoTotal = 0;
+  let vMaxPrecioLiq = 0; //Inicializo en 0 pues se que no puede haber precios negativos
+  let vMaxPrecioLiqMarca = "No se ingreso";
+  let vMinPrecioSol;
+  let vMinPrecioSolMarca = "No se ingreso";
+  let vBanderaMin = false;
+
+  do{
+
+    vMarca = prompt("Ingrese la marca");
+
+    do{
+    vPrecio = parseFloat(prompt("Ingrese el precio"));
+    }while(vPrecio<0 || isNaN(vPrecio))
+
+    do{
+      vPeso= parseFloat(prompt("Ingrese el peso"));
+    }while(vPeso<0 || isNaN(vPeso))
+
+    do{
+      vTipo = prompt("Ingrese el tipo (sólido/líquido)");
+    }while(vTipo!= "sólido" && vTipo!= "líquido")
+
+    vAcumPesoTotal+=vPeso;
+    
+    switch(vTipo){
+      case "sólido":
+        if(vBanderaMin == false){
+          vMinPrecioSol = vPrecio;
+          vMinPrecioSolMarca = vMarca;
+          vBanderaMin = true;
+
+        }else if(vMinPrecioSol>vPrecio){
+          vMinPrecioSol = vPrecio;
+          vMinPrecioSolMarca = vMarca;
+        }
+
+      break;
+      case "líquido":
+
+        if(vMaxPrecioLiq<vPrecio){
+          vMaxPrecioLiq= vPrecio;
+          vMaxPrecioLiqMarca = vMarca;
+        }
+
+      break;
+
+    }
+
+
+    vRespuesta = prompt("¿Desea continuar?");
+
+  }while(vRespuesta == "si")
+
+
+  alert("a)El peso total de la compra es de: " + vAcumPesoTotal + ".\nb)La marca del más caro de los líquidos es: " + vMaxPrecioLiqMarca +
+  ".\c)La marca del más barato de los sólidos es: " + vMinPrecioSolMarca);
 }
--- uno.js
+++ uno.js
@@ -9,5 +9,51 @@ pedir datos por prompt y mostrar por document.write o console.log
 */
 function mostrar()
 {
-	alert("uno");
+	let vNombre;
+	let vPeso;
+	let vEdad;
+	let vSexo;
+	const cCantidadPersonas = 5;
+	let vContMujeres = 0;
+	let vAcumEdad = 0 ;
+	let vMaxPesoHombre = 0; // Inicializo en 0 pues no hay pesos negativos
+	let vMaxPesoHombreNombre;
+	
+
+	for(let i=1; i<= cCantidadPersonas; i++){
+
+	vNombre= prompt("Ingrese el nombre");
+
+	do{
+	vSexo = prompt("Ingrese el sexo(f/m)");
+	}while(vSexo!="f" && vSexo!="m")
+
+	do{
+	vPeso = parseFloat(prompt("Ingrese el peso"));
+	}while(vPeso<0 || isNaN(vPeso))
+
+	do{
+	vEdad = parseInt(prompt("Ingrese la edad"));
+	}while(vEdad<0 || isNaN(vEdad))
+
+
+	if(vSexo == "f"){
+
+		vContMujeres++;
+
+	}else{
+		if(vMaxPesoHombre<vPeso){
+			vMaxPesoHombre = vPeso;
+			vMaxPesoHombreNombre = vNombre;
+		}
+
+	}
+	
+	vAcumEdad+=vEdad;
+
+	}
+
+	document.write("a)La cantidad de mujeres ingresadas es: " + vContMujeres + ".</br>b)La edad promedio en total es: " + (vAcumEdad/cCantidadPersonas)+
+	".</br>c)El hombre mas pesado es: " + vMaxPesoHombreNombre )
+
 }

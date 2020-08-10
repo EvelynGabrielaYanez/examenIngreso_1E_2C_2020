0ab66b3f0daa9a9519b27ba37b9399efc3038f83
EvelynYanez <evelyn.gabriela.yanez@gmail.com>
2020-08-10 20:22:04 -0300

    ejercicio 3

--- tres.js
+++ tres.js
@@ -9,7 +9,85 @@ a)el lugar más elegido
 b)el sexo de titular que lleva más pasajeros.
 c)el promedio de personas por viaje,  que viajan en invierno
 */
-function mostrar()
-{
-	alert("tres");
+function mostrar() {
+	let vSexoTitular;
+	let vLugar;
+	let vTemporada;
+	let vCantidadPersonas;
+	let vRespuesta;
+	let vContBariloche = 0;
+	let vContCataratas = 0;
+	let vContSalta = 0;
+	let vLugarMasElegido;
+	let vMaxPasajeros = 0; //Inicializo en 0 pues se que no puede haber una cantidad negativa de pasajeros
+	let vMaxPasajerosSexo;
+	let vAcumInvierno = 0;
+	let vContInvierno = 0;
+	let vPromedioInvierno;
+
+
+	do {
+		do {
+			vSexoTitular = prompt("Ingrese el sexo del titular(f/m)");
+		} while (vSexoTitular != "f" && vSexoTitular != "m")
+
+		do {
+			vLugar = prompt("Ingrese el lugar");
+		} while (vLugar != "bariloche" && vLugar != "cataratas" && vLugar != "salta")
+
+		do {
+			vTemporada = prompt("Ingrese la temporada");
+		} while (vTemporada != "otoño" && vTemporada != "verano" && vTemporada != "primavera"  && vTemporada != "invierno")
+
+		do {
+			vCantidadPersonas = parseInt(prompt("Ingrese la cantidad de personas"));
+		} while (vCantidadPersonas <= 0 || isNaN(vCantidadPersonas))
+
+		switch (vLugar) {
+			case "bariloche":
+				vContBariloche++;
+
+				break;
+			case "cataratas":
+				vContCataratas++;
+				break;
+			case "salta":
+				vContSalta++;
+				break;
+		}
+
+		if(vMaxPasajeros<vCantidadPersonas){
+			vMaxPasajeros = vCantidadPersonas;
+			vMaxPasajerosSexo = vSexoTitular;
+		}
+
+		if(vTemporada == "invierno"){
+			vAcumInvierno+=vCantidadPersonas;
+			vContInvierno++;
+		}
+	
+		vRespuesta = prompt("¿Desea continuar?(si/no)");
+	} while (vRespuesta == "si")
+
+
+	if(vContBariloche>vContCataratas && vContBariloche> vContSalta){
+		vLugarMasElegido = "Bariloche";
+	}else if(vContCataratas>vContBariloche && vContCataratas>vContSalta){
+		vLugarMasElegido = "Cataratas";
+	}else{
+		vLugarMasElegido = "Salta";
+	}
+
+	if(vContInvierno==0){
+
+		vPromedioInvierno = "No se registraron viajes en invierno";
+		
+	}else{
+		vPromedioInvierno =	vAcumInvierno/vContInvierno;
+	}
+
+	
+	alert("a)El lugar más elegido es: " + vLugarMasElegido + ".\nb)El sexo del titular que lleva más pasajeros es: " + vMaxPasajerosSexo + 
+	"\nc)El promedio de personas por viaje, que viajan en inverno es: " + vPromedioInvierno );
+
 }
